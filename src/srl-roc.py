6b78c037ba30ba66d7e92064f5e7654cadbaec1b
Omar El Malki <homerdon0@gmail.com>
2022-07-27 18:08:27 +0200

    Add faster SRL script with batched prediction

---
+++ src/srl-roc.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import sys
+import os
+import json
+from tqdm import tqdm
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'utils'))
+import utils.semantic_role_labeling as sem_rl
+
+# Read ROCStories into pandas DataFrame
+roc_stories_path_csv = '../generated/coreference_resolution/ROCStories_with_resolved_coreferences.csv'
+roc_stories_df = pd.read_csv(roc_stories_path_csv, sep='\t', header=0)
+
+print(len(roc_stories_df))
+
+predictor = sem_rl.get_predictor()
+batch_size = 128
+
+for n in range(1, 6):
+    batch_data = []
+    batch_result = []
+
+    for index, row in tqdm(roc_stories_df.iterrows()):
+        line = str(row[f'resolved{n}'])
+        if not line.isspace():
+            line = {"sentence": line.strip()}
+            line = json.dumps(line)
+            json_data = predictor.load_line(line)
+            batch_data.append(json_data)
+            if len(batch_data) == batch_size:
+                res = sem_rl.run_batch_predictor(batch_data, predictor)
+                for b in res:
+                    batch_result.append(b)
+                batch_data = []
+
+    if batch_data:
+        res = sem_rl.run_batch_predictor(batch_data, predictor)
+        for b in res:
+            batch_result.append(b)
+
+    roc_stories_df[f'srl_r{n}'] = batch_result
+
+srl_df = roc_stories_df
+
+# Convert Dataframe to csv
+srl_df.to_csv('../generated/prop-bank-entity-extraction/ROCStories_resolved_srl.csv', sep='\t')
--- src/utils/semantic_role_labeling.py
+++ src/utils/semantic_role_labeling.py
@@ -18,3 +18,22 @@ def sentence_to_srl(sentence: str) -> JsonDict:
     # More documentation: https://docs.allennlp.org/models/main/models/structured_prediction/predictors/srl/
     semantic_roles: JsonDict = predictor.predict(sentence)
     return semantic_roles
+
+
+def get_predictor():
+    """
+    Return SRL transformer predictor
+    :return:
+    """
+    return predictors.SrlTransformersPredictor.from_path(
+        "../data/pre-trained-transformer-srl/srl_bert_base_conll2012.tar.gz", "transformer_srl")
+
+
+def run_batch_predictor(batch_data, predictor):
+    if len(batch_data) == 1:
+        result = predictor.predict_json(batch_data[0])
+        results = [result]
+    else:
+        results = predictor.predict_batch_json(batch_data)
+    return results
+
